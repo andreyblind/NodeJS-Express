9820a16d3c0e85f5ceb839f6367a907194ab7f8d
Andrii Khanchuk <andreyblind@gmail.com>
2016-07-22 23:43:23 +0300

    base app structure is ready

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var socketio = require('socket.io');
-
+var messages = [];
 var app = express();
 app.use('/static', express.static('static'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var server = app.listen(8000);
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + "/static/index_ajax.html");
 });
+
+app.get('/messages', function (req, res) {
+	res.json(messages);
+});
+
+app.post('/messages', function (req, res) {
+	var message = req.body;
+	messages.push(message);
+	res.json(message);
+});
+
