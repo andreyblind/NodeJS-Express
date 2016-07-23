4899a06c107eb7dab9dde857b20d3ca98f577c85
Andrii Khanchuk <andreyblind@gmail.com>
2016-07-23 15:48:31 +0300

    messages pushed to server via socket

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ var socketio = require('socket.io');
 
 var messages = [];
 var app = express();
+var server = app.listen(8000);
+var io = socketio.listen(server);
 app.use('/static', express.static('static'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var server = app.listen(8000);
+
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + "/static/index_ajax.html");
@@ -26,5 +28,21 @@ app.post('/messages', function (req, res) {
 
 app.get('/sockets', function (req, res) {
 	res.sendfile(__dirname + "/static/index_socket.html");
+});
+
+io.on('connection', function(socket) {
+
+	console.info('Client connected');
+
+	socket.on('disconnected', function() {
+		console.log('Client disconnected');
+	});
+
+	socket.on('chat message', function(msg) {
+		messages.push(msg);
+		io.emit('chat message', msg);
+	});
+
+	socket.emit('chat history', messages);
 });
 
